643e75250a6f889b20e5a0e1ca83d9ecb81b996e
Austin Kregel <austinkregel@gmail.com>
2016-06-06 10:33:00 -0400

    Add ziptastic test

---
+++ tests/ZiptasticTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Kregel\Ziptastic\Ziptastic;
+
+class ZiptasticTest extends PHPUnit_Framework_TestCase
+{
+    protected $key = '';
+
+    public function testZipper()
+    {
+        $results = (new Ziptastic(23042,'us'))->setKey($this->key)->find();
+
+    }
+}
