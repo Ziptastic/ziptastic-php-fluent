06be72ec0d27b5c2ac0617c3b491740df13c612f
Austin <austinkregel@users.noreply.github.com>
2016-06-21 23:44:49 -0400

    Update documentation

--- src/Zipper.php
+++ src/Zipper.php
@@ -10,7 +10,7 @@ class Zipper extends ZiptasticRequest
     /**
      * This sets the country code for the call.
      *
-     * @param $country
+     * @param string $country
      *
      * @return Zipper
      */
@@ -25,8 +25,8 @@ class Zipper extends ZiptasticRequest
      * Determine what the developer is trying to call on. If it doesn't exist throw an
      * exception to let them know their variable cannot be found.
      *
-     * @param $name
-     * @param $arguments
+     * @param string $name
+     * @param array $arguments
      *
      * @throws Exception
      *
@@ -34,6 +34,9 @@ class Zipper extends ZiptasticRequest
      */
     public function __call($name, $arguments)
     {
+        // Check to see if the method called is able to be resolved,
+        // Current supported method are with(), andWidth(), and and()
+        // If the method cannot be resolved, throw an exception
         if (substr($name, 0, 4) === 'with') {
             return $this->resolveWith($name, $arguments);
         } elseif (substr(strtolower($name), 0, 7) === 'andwith') {
@@ -48,16 +51,23 @@ class Zipper extends ZiptasticRequest
      * @param $with
      * @param $args
      *
-     * @return bool|Zipper
+     * @return Zipper
      */
     private function resolveWith($with, $args)
     {
+        // Remove the word "with" so we can get what we really want, the
+        // function or the attribute we really want.
         $variable = (substr($with, 4, strlen($with)));
 
+        // Split the capital and lower cased words, so if someone passes a PostalCode
+        // We can resolve it to postal_code
         $parts = preg_split('/(?<=[a-z])(?=[A-Z])/x', $variable);
-
+        
+        // Here we change the array of parts into just one word and glue them together with
+        // and underscore, which follows the desired name convention.
         $variable = strtolower(implode('_', $parts));
-
+        
+        // Call and return with
         return $this->with($variable, $args);
     }
 
@@ -67,11 +77,15 @@ class Zipper extends ZiptasticRequest
      * @param $variable
      * @param $value
      *
-     * @return $this
+     * @return Zipper
      */
     public function with($variable, $value)
     {
+        // If the variable is an array with a singel value we just want that value, 
+        // So we grab the first element and set it equal to the value. Otherwise we just
+        // Pass the value through.
         if (is_array($value)) {
+            // If the count is more than 1 we should assume that we're dealing with coordinates.
             if (count($value) == 1) {
                 $value = $value[0];
             }
